ef30fcabb662ec68838006c0d103e1560df514ec
Salman <slmnakber@gmail.com>
2026-08-01 17:52:02 +0500

    s

--- wp-content/themes/montTheme/woocommerce/size-guide-drawer.php
+++ wp-content/themes/montTheme/woocommerce/size-guide-drawer.php
@@ -22,38 +22,13 @@
 							</tr>
 						</thead>
 						<tbody>
-							<tr><td>Neck/Collar</td><td>39</td><td>41</td><td>43</td><td>45</td><td>47</td><td>49</td></tr>
-							<tr><td>Chest Width</td><td>110</td><td>108</td><td>118</td><td>126</td><td>134</td><td>142</td></tr>
-							<tr><td>Waist Width</td><td>94</td><td>102</td><td>110</td><td>118</td><td>124</td><td>132</td></tr>
-							<tr><td>Bottom Width</td><td>100</td><td>108</td><td>116</td><td>124</td><td>132</td><td>140</td></tr>
-							<tr><td>Shoulder Across</td><td>44</td><td>46</td><td>48.5</td><td>51.5</td><td>54.5</td><td>57.5</td></tr>
-							<tr><td>Sleeve Length</td><td>64.5</td><td>66.5</td><td>68.5</td><td>70.5</td><td>72.5</td><td>74.5</td></tr>
-							<tr><td>Back Length</td><td>74.5</td><td>75.5</td><td>77.5</td><td>79.5</td><td>81</td><td>83</td></tr>
-						</tbody>
-					</table>
-				</div>
-			</section>
-
-			<section class="monte-size-chart">
-				<span class="monte-size-chart__badge">Slim Fit</span>
-				<p class="monte-size-chart__subtitle">Size Charts (cm)</p>
-				<div class="monte-size-table-wrap">
-					<table>
-						<thead>
-							<tr>
-								<th>SIZE</th>
-								<th>S / 37</th><th>S / 38</th><th>M / 39</th><th>M / 40</th>
-								<th>L / 41</th><th>L / 42</th><th>XL / 43</th><th>XL / 44</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr><td>Neck/Collar</td><td>37</td><td>38</td><td>39</td><td>40</td><td>41</td><td>42</td><td>43</td><td>44</td></tr>
-							<tr><td>Chest Width</td><td>96</td><td>102</td><td>105</td><td>109</td><td>113</td><td>118</td><td>123</td><td>126</td></tr>
-							<tr><td>Waist Width</td><td>86</td><td>91</td><td>94</td><td>99</td><td>103</td><td>106</td><td>110</td><td>113</td></tr>
-							<tr><td>Bottom Width</td><td>96</td><td>100</td><td>104</td><td>108</td><td>112</td><td>117</td><td>122</td><td>125</td></tr>
-							<tr><td>Shoulder Across</td><td>42.5</td><td>44</td><td>45.5</td><td>47</td><td>48</td><td>49</td><td>51</td><td>52</td></tr>
-							<tr><td>Sleeve Length</td><td>64.5</td><td>65</td><td>65.5</td><td>66</td><td>67</td><td>68</td><td>69</td><td>69</td></tr>
-							<tr><td>Back Length</td><td>77</td><td>78</td><td>79</td><td>80</td><td>81</td><td>82</td><td>83</td><td>84</td></tr>
+							<tr><td>Neck/Collar</td><td>38</td><td>40</td><td>42</td><td>44</td><td>46</td><td>48</td></tr>
+							<tr><td>Chest Width</td><td>53</td><td>56.5</td><td>61</td><td>65</td><td>67</td><td>73</td></tr>
+							<tr><td>Waist Width</td><td>47.5</td><td>51.5</td><td>55.5</td><td>59.5</td><td>62.5</td><td>68</td></tr>
+							<tr><td>Bottom Width</td><td>51.5</td><td>55.5</td><td>59.5</td><td>63.5</td><td>66.5</td><td>73</td></tr>
+							<tr><td>Shoulder Across</td><td>44.5</td><td>46.5</td><td>49</td><td>52</td><td>55</td><td>58</td></tr>
+							<tr><td>Sleeve Length</td><td>64.5</td><td>66.5</td><td>68.5</td><td>70.5</td><td>71.5</td><td>74.5</td></tr>
+							<tr><td>Back Length</td><td>76.5</td><td>77.5</td><td>79</td><td>81</td><td>82.5</td><td>85</td></tr>
 						</tbody>
 					</table>
 				</div>
@@ -74,8 +49,8 @@
 						<tbody>
 							<tr><td>Neck/Collar</td><td>39</td><td>40</td><td>41</td><td>42</td><td>43</td><td>44</td><td>45</td><td>46</td></tr>
 							<tr><td>Chest Width</td><td>110</td><td>114</td><td>116</td><td>120</td><td>124</td><td>127</td><td>132</td><td>135</td></tr>
-							<tr><td>Waist Width</td><td>100</td><td>103</td><td>106</td><td>109</td><td>112</td><td>115</td><td>118</td><td>123</td></tr>
-							<tr><td>Bottom Width</td><td>109</td><td>112</td><td>115</td><td>118</td><td>125</td><td>130</td><td>133</td><td>136</td></tr>
+							<tr><td>Waist Width</td><td>100</td><td>103</td><td>106</td><td>109</td><td>114</td><td>118</td><td>121</td><td>124</td></tr>
+							<tr><td>Bottom Width</td><td>109</td><td>112</td><td>115</td><td>120</td><td>125</td><td>130</td><td>133</td><td>136</td></tr>
 							<tr><td>Shoulder Across</td><td>46.5</td><td>48</td><td>49.5</td><td>51</td><td>53.5</td><td>55</td><td>56.5</td><td>58</td></tr>
 							<tr><td>Sleeve Length</td><td>66</td><td>66.5</td><td>67</td><td>67.5</td><td>68</td><td>69</td><td>69.5</td><td>70</td></tr>
 							<tr><td>Back Length</td><td>79</td><td>80</td><td>81</td><td>82</td><td>83</td><td>84</td><td>85</td><td>86</td></tr>
@@ -85,25 +60,25 @@
 			</section>
 
 			<section class="monte-size-chart">
-				<span class="monte-size-chart__badge">Loose Fit</span>
+				<span class="monte-size-chart__badge">Slimfit</span>
 				<p class="monte-size-chart__subtitle">Size Charts (cm)</p>
 				<div class="monte-size-table-wrap">
 					<table>
 						<thead>
 							<tr>
 								<th>SIZE</th>
-								<th>M / 39-40</th><th>L / 41-42</th><th>XL / 43-44</th>
-								<th>2XL / 45-46</th><th>3XL / 47-48</th><th>4XL / 49-50</th>
+								<th>XS / 36</th><th>S / 37</th><th>S / 38</th><th>M / 39</th><th>M / 40</th>
+								<th>L / 41</th><th>L / 42</th><th>XL / 43</th><th>XL / 44</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr><td>Neck/Collar</td><td>40</td><td>42</td><td>44</td><td>46</td><td>48</td><td>50</td></tr>
-							<tr><td>Chest Width</td><td>118</td><td>126</td><td>133</td><td>140</td><td>147</td><td>154</td></tr>
-							<tr><td>Waist Width</td><td>113</td><td>121</td><td>130</td><td>138</td><td>146</td><td>153</td></tr>
-							<tr><td>Bottom Width</td><td>117</td><td>125</td><td>132</td><td>140</td><td>147</td><td>154</td></tr>
-							<tr><td>Shoulder Across</td><td>52</td><td>55</td><td>58</td><td>61</td><td>64</td><td>67</td></tr>
-							<tr><td>Sleeve Length</td><td>89</td><td>92</td><td>94</td><td>95</td><td>98</td><td>99</td></tr>
-							<tr><td>Back Length</td><td>81</td><td>83</td><td>84</td><td>85</td><td>87</td><td>89</td></tr>
+							<tr><td>Neck/Collar</td><td>36</td><td>37</td><td>38</td><td>39</td><td>40</td><td>41</td><td>42</td><td>43</td><td>44</td></tr>
+							<tr><td>Chest Width</td><td>93</td><td>96</td><td>102</td><td>105</td><td>109</td><td>113</td><td>118</td><td>123</td><td>126</td></tr>
+							<tr><td>Waist Width</td><td>83</td><td>86</td><td>91</td><td>94</td><td>99</td><td>103</td><td>106</td><td>110</td><td>113</td></tr>
+							<tr><td>Bottom Width</td><td>92</td><td>96</td><td>100</td><td>104</td><td>108</td><td>112</td><td>117</td><td>122</td><td>125</td></tr>
+							<tr><td>Shoulder Across</td><td>41</td><td>42.5</td><td>44</td><td>45.5</td><td>47</td><td>48</td><td>49</td><td>51</td><td>52</td></tr>
+							<tr><td>Sleeve Length</td><td>64</td><td>64.5</td><td>65</td><td>65.5</td><td>66</td><td>67</td><td>68</td><td>69</td><td>69.5</td></tr>
+							<tr><td>Back Length</td><td>75</td><td>77</td><td>78</td><td>79</td><td>80</td><td>81</td><td>82</td><td>83</td><td>84</td></tr>
 						</tbody>
 					</table>
 				</div>
